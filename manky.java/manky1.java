624eaeeb99e4e430a74e07560cc16fb49dc94593
Joshua Abraham <joshua.r.abraham@outlook.com>
2026-09-19 17:58:38 -0700

    new additiion to program to simulate a simple system that gives you logical outputs based on your inputs. The program will take in a series of inputs and will output a logical response based on the inputs provided. The program will be able to handle multiple inputs and will provide a response for each input. The program will also be able to handle invalid inputs and will provide an error message for any invalid input. The program will be able to run in a loop, allowing the user to continue providing inputs until they choose to exit the program.

--- manky.java/manky1.java
+++ manky.java/manky1.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 public class manky1 {
     public static void main(String[] args) {
         try (Scanner kybd = new Scanner(System.in)) {
-            System.out.println("Enter a number:");
+            System.out.println("This is a program that predicts your future based on your input.");
+            System.out.println("Do you want to continue? (yes/no)");
+            if (!kybd.nextLine().equalsIgnoreCase("yes")) {
+                System.out.println("Goodbye!");
+                return;
+            }
             int n = kybd.nextInt();
             System.out.println("You entered: " + n);
         }
